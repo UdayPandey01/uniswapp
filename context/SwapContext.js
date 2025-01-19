4e425a23c425b684e0f3755cf67870f496017200
UdayPandey01 <pandeyuday113@gmail.com>
2025-01-19 12:49:55 +0530

    added swapfunctionality

--- context/SwapContext.js
+++ context/SwapContext.js
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useEffect, useState } from "react";
+import React, { use, useEffect, useState } from "react";
 import { ethers, BigNumber } from "ethers";
 import Web3Modal from "web3modal";
 
@@ -22,7 +22,7 @@ export const SwapTokenContext = React.createContext();
 export const SwapTokenContextProvider = ({ children }) => {
   const swap = "Welcome to swap my token ";
 
-  const [accounts, setAccounts] = useState("");
+  const [account, setAccount] = useState("");
   const [ether, setEther] = useState("");
   const [networkConnected, setNetworkConnected] = useState("");
   const [weth9, setWeth9] = useState("");
@@ -31,16 +31,16 @@ export const SwapTokenContextProvider = ({ children }) => {
   const [tokenData, setTokenData] = useState([]);
 
   const addToken = [
-    "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",
-    "0x905Ad472d7eeB94ed1Fc29D8ff4B53FD4D5a5Eb4",
-    "0xe3EF345391654121f385679613Cea79A692C2Dd8",
+    // "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",
+    "0x1EFC0069D5BAbEF06a89d235D85485835dF2966C",
+    "0x1ec26635096fd7E3B7f27Ff8D76E1518a134b478",
   ];
 
   const fetchingData = async () => {
     try {
       const userAccount = await checkIfWalletIsConnected();
-      setAccounts(userAccount);
-      console.log("User Account: ", userAccount);
+      setAccount(userAccount);
+    //   console.log("User Account: ", userAccount);
 
       const web3Modal = new Web3Modal();
       const connection = await web3Modal.connect();
@@ -48,17 +48,65 @@ export const SwapTokenContextProvider = ({ children }) => {
 
       const balance = await provider.getBalance(userAccount);
       const balanceInEther = ethers.formatEther(balance);
-      console.log("Balance: ", balanceInEther);
+    //   console.log("Balance: ", balanceInEther);
       setEther(balanceInEther);
+
+      const network = await provider.getNetwork();
+      setNetworkConnected(network.name);
+    //   console.log("Network : ", network);
 
       addToken.map(async (el, i) => {
         const contract = new ethers.Contract(el, ERC20ABI, provider);
 
         const _userBalance = await contract.balanceOf(userAccount); //balanceOf method is a part of ERC20
-        console.log(`userBalance for contract ${el} : `, _userBalance);
+        // console.log(`userBalance for contract ${el} : `, _userBalance);
         const userBalanace = ethers.formatEther(_userBalance);
-        console.log(`userBalance for contract ${el} : `, userBalanace);
+        // console.log(`userBalance for contract ${el} : `, userBalanace);
+
+        const symbol = await contract.symbol();
+        const name = await contract.name();
+
+        tokenData.push({
+          name: name,
+          symbol: symbol,
+          userBalanace: userBalanace,
+        });
+
+        // console.log("Token Data: ", tokenData);
       });
+
+      try {
+        const daiContract = await connectingToDAIToken();
+        // console.log(daiContract.target);
+        if (!daiContract) {
+          throw new Error("DAI contract instance is not available.");
+        }
+        if (!ethers.isAddress(userAccount)) {
+          throw new Error("Invalid address provided.");
+        }
+        const daiBal = await daiContract.balanceOf(userAccount);
+        const daiBalance = ethers.formatEther(daiBal);
+        // console.log(`DAI Balance : ${daiBalance}`);
+        setDai(daiBalance);
+
+        // //WETH BALANCE
+        // const wethContract = await connectingToIWETHToken();
+        // console.log("Weth contract",wethContract.target);
+        // if (!wethContract) {
+        //   throw new Error("WETH contract instance is not available.");
+        // }
+        // if (!ethers.isAddress(userAccount)) {
+        //   throw new Error("Invalid address provided.");
+        // }
+        // console.log("1111")
+        // const wethBal = await wethContract.balanceOf(userAccount);
+        // const wethBalance = ethers.formatEther(wethBal);
+        // console.log(`WETH Balance : ${wethBalance}`);
+        // setWeth9(wethBalance);
+      } catch (error) {
+        console.error(error);
+      }
+
     } catch (error) {
       console.log(error);
     }
@@ -68,8 +116,41 @@ export const SwapTokenContextProvider = ({ children }) => {
     fetchingData();
   }, []);
 
+  const singleSwapToken = async () => {
+    try {
+        let singleSwapToken
+        let weth
+        let dai
+
+        singleSwapToken = await connectingToSingleSwapToken()
+        console.log("SingleSwapToken",singleSwapToken)
+        weth = await connectingToIWETHToken()
+        console.log("WETH",weth)
+        dai = await connectingToDAIToken()
+        console.log("DAI",dai)
+
+        const amountIn = 10n ** 18n
+
+        await weth.deposit({value: amountIn})
+        await weth.approve(singleSwapToken.target, amountIn)
+
+        await singleSwapToken.swapExactInputSingle(amountIn,{
+            gasLimit: 3000000,
+        })
+
+        const balance = await dai.balanceOf(account)
+        const transferAmount = BigNumber.from(balance).toString()
+        const ethValue = ethers.utils.formatEther(transferAmount)
+        setDai(ethValue)
+
+        console.log("DAI Balance: ",ethValue)
+    } catch (error) {
+        console.log(error)
+    }
+  }
+
   return (
-    <SwapTokenContext.Provider value={{ swap }}>
+    <SwapTokenContext.Provider value={{singleSwapToken,connectWalelt,tokenData, account, ether, networkConnected, weth9, dai }}>
       {children}
     </SwapTokenContext.Provider>
   );
--- context/constants.js
+++ context/constants.js
@@ -2,27 +2,27 @@ import booToken from "./BooToken.json";
 import lifeToken from "./LifeToken.json";
 import singleSwapToken from "./SingleSwapToken.json";
 import swapMultiHop from "./SwapMultiHop.json";
-import IWETH from "./IWETH.json";
+// import IWETH from "./IWETH.json";
 import ERC20 from "./ERC20.json";
+import WETH from "./WethToken.json";
+import DAI from "./DaiToken.json";
 
-export const BooTokenAddress = "0x905Ad472d7eeB94ed1Fc29D8ff4B53FD4D5a5Eb4";
+export const BooTokenAddress = "0x1EFC0069D5BAbEF06a89d235D85485835dF2966C";
 export const BooTokenAbi = booToken.abi;
 
-export const LifeTokenAddress = "0xe3EF345391654121f385679613Cea79A692C2Dd8"
+export const LifeTokenAddress = "0x1ec26635096fd7E3B7f27Ff8D76E1518a134b478"
 export const LifeTokenAbi = lifeToken.abi;
 
-export const SingleSwapTokenAddress = "0xC070A317F23E9A4e982e356485416251dd3Ed944"
+export const SingleSwapTokenAddress = "0xFF899d024791593802e39b05DDBAD7ea36ad9612"
 export const SingleSwapTokenAbi = singleSwapToken.abi;
 
-export const SwapMultiHopAddress = "0x6D39d71fF4ab56a4873febd34e1a3BDefc01b41e"
+export const SwapMultiHopAddress = "0x54d3eDd746B34285a2A2ad48C174D855CbbA0b9B"
 export const SwapMultiHopAbi = swapMultiHop.abi;
 
-export const IWETHAddress = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2"
-export const IWETHAbi = IWETH.abi;
+export const WETHAddress = "0xf4dB42018d7B439258c2529418Db8E08A11AE559"
+export const WETHAbi = WETH.abi;
+
+export const DAIAddress = "0xF49f5bc0C11a4c2b77E01767779307e3a683fFF9"
+export const DAIAbi = DAI.abi;
 
 export const ERC20ABI = ERC20.abi;
-
-// BooToken deployed to: 0x905Ad472d7eeB94ed1Fc29D8ff4B53FD4D5a5Eb4
-// LifeToken deployed to: 0xe3EF345391654121f385679613Cea79A692C2Dd8
-// SingleSwapToken deployed to: 0xC070A317F23E9A4e982e356485416251dd3Ed944
-// SwapMultiHop deployed to: 0x6D39d71fF4ab56a4873febd34e1a3BDefc01b41e
