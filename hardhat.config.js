806d09baacf08e5b9dee8f5c7b2477570201a0c9
UdayPandey01 <pandeyuday113@gmail.com>
2025-01-19 12:48:14 +0530

    added base sepolia network

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,9 +1,18 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
+const BASE_SEPOLIA_RPC_URL = process.env.BASE_SEPOLIA_RPC_URL;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
 module.exports = {
   solidity: "0.8.28",
   networks : {
+    base_sepolia: {
+      url: BASE_SEPOLIA_RPC_URL,
+      accounts: [PRIVATE_KEY],
+      chainId: 84532,
+      blockConfirmations : 6
+    },
     hardhat : {
       forking : {
         url : "https://eth-mainnet.g.alchemy.com/v2/lDKVERTRp5nBVVuFoE6jKjTIYGBGV05S"
